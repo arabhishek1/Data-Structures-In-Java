8a68db02d730aeca70aadb8d8a391cc680c1d4f2
Abhishek A R <mail2arabhishek@gmail.com>
2018-09-14 12:15:01 +0530

    dfs cehck

--- src/Graph/ReplaceOsWithXs.java
+++ src/Graph/ReplaceOsWithXs.java
@@ -77,6 +77,7 @@ public class ReplaceOsWithXs {
                     arr[currentRow][currentCol] = 'X';
                     dfs(arr, visited, currentRow, currentCol);
                 }
+                visited[currentRow][currentCol] = true;
             }
         }
     }
@@ -96,6 +97,12 @@ public class ReplaceOsWithXs {
                         {'X', 'X', 'X', 'O', 'X', 'O'},
                         {'O', 'O', 'X', 'O', 'O', 'O'}};
 
+        for(int i=0; i < mat.length; ++i){
+            for(int j=0; j < mat[0].length; ++j)
+                System.out.print(mat[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
         replaceOswithXs(mat);
         for(int i=0; i < mat.length; ++i){
             for(int j=0; j < mat[0].length; ++j)
